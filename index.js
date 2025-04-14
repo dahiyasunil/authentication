02dc81239f835d7e251eca3086fe47c4a9c03e0f
dahiyasunil <sunil.dahiya13@gmail.com>
2025-04-14 22:06:01 +0530

    basic configurations like dotenv, cors, urlencoding and json middleware

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+const corsOptions = {
+  origin: [process.env.BASE_URL, "http://localhost:3000"],
+  credentials: true,
+  methods: ["GET", "POST", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
